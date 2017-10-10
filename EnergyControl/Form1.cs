3b97aeab518f8f07588371e1af3ebc772ae7660f
BloodDecember <egar@list.ru>
2017-10-10 22:10:15 +0300

    Добавляет дату в таблицу.

--- EnergyControl/Form1.cs
+++ EnergyControl/Form1.cs
@@ -1,20 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EnergyControl
 {
     public partial class Form1 : Form
+
     {
+        static string ApplicationName = "Update Google Sheet Data with Google Sheets API v4";
+        static String spreadsheetId = "1xFHgjLLInDenc3g1UpydfGET6nViTcsgu0v7Rr8fl7k";
+        static string sheetName = "Лист1";
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime dateOnly = DateTime.Now;
+            textBox1.Text = dateOnly.ToString("d");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var service = OpenSheet();
+            UpdateRow(service, textBox1.Text);
+        }
+
+        static SheetsService OpenSheet()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Path.Combine
+                    (System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                     ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            return service;
+        }
+
+        static void UpdateRow(SheetsService service, string textBox)
+        {
+            ValueRange rVR;
+            String sRange;
+            int rowNumber = 1;
+
+            sRange = String.Format("{0}!A:A", sheetName);
+            SpreadsheetsResource.ValuesResource.GetRequest getRequest
+                = service.Spreadsheets.Values.Get(spreadsheetId, sRange);
+            rVR = getRequest.Execute();
+            IList<IList<Object>> values = rVR.Values;
+
+            if (values != null && values.Count > 0) rowNumber = values.Count + 1;
+            sRange = String.Format("{0}!A{1}:B{1}", sheetName, rowNumber);
+
+            ValueRange valueRange = new ValueRange();
+            valueRange.Range = sRange;
+            valueRange.MajorDimension = "ROWS";
+
+            DateTime dt = new DateTime();
+            dt = DateTime.Now;
+            List<object> oblist = new List<object>() { textBox, String.Format("{0}", rowNumber)};
+            valueRange.Values = new List<IList<object>> { oblist };
+            Console.WriteLine("{0}, {1}", oblist[0], oblist[1]);
+
+            SpreadsheetsResource.ValuesResource.UpdateRequest updateRequest
+                = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, sRange);
+            updateRequest.ValueInputOption
+                = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+            UpdateValuesResponse uUVR = updateRequest.Execute();
         }
     }
 }
