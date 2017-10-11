0e9b8e15273d0ff873e487356897fb7bee75ba28
Пудовкин Егор Николаевич <blooddecember@gmail.com>
2017-10-11 13:55:57 +0300

    Дата и значения обновляются.

--- EnergyControl/Form1.cs
+++ EnergyControl/Form1.cs
@@ -32,12 +32,19 @@ namespace EnergyControl
         {
             DateTime dateOnly = DateTime.Now;
             textBox1.Text = dateOnly.ToString("d");
+
+            var service = OpenSheet();
+            textBox2.Text = GetPreDate(service, "B1:B");
+            textBox4.Text = GetPreDate(service, "A1:A");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             var service = OpenSheet();
             UpdateRow(service, textBox1.Text);
+
+            textBox2.Text = GetPreDate(service, "B1:B");
+            textBox4.Text = GetPreDate(service, "A1:A");
         }
 
         static SheetsService OpenSheet()
@@ -55,7 +62,6 @@ namespace EnergyControl
                     "user",
                     CancellationToken.None,
                     new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
             }
 
             var service = new SheetsService(new BaseClientService.Initializer()
@@ -66,11 +72,12 @@ namespace EnergyControl
             return service;
         }
 
-        static void UpdateRow(SheetsService service, string textBox)
+        string UpdateRow(SheetsService service, string textBox)
         {
             ValueRange rVR;
             String sRange;
             int rowNumber = 1;
+            String rowNumberString;
 
             sRange = String.Format("{0}!A:A", sheetName);
             SpreadsheetsResource.ValuesResource.GetRequest getRequest
@@ -89,13 +96,41 @@ namespace EnergyControl
             dt = DateTime.Now;
             List<object> oblist = new List<object>() { textBox, String.Format("{0}", rowNumber)};
             valueRange.Values = new List<IList<object>> { oblist };
-            Console.WriteLine("{0}, {1}", oblist[0], oblist[1]);
+            //Console.WriteLine("{0}, {1}", oblist[0], oblist[1]);
 
-            SpreadsheetsResource.ValuesResource.UpdateRequest updateRequest
+            rowNumber = rowNumber - 1;
+            rowNumberString = "A" + rowNumber.ToString();
+            if (oblist[0].ToString() != GetPreDate(service, rowNumberString))
+            {
+                SpreadsheetsResource.ValuesResource.UpdateRequest updateRequest
                 = service.Spreadsheets.Values.Update(valueRange, spreadsheetId, sRange);
-            updateRequest.ValueInputOption
+                updateRequest.ValueInputOption
                 = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
-            UpdateValuesResponse uUVR = updateRequest.Execute();
+                UpdateValuesResponse uUVR = updateRequest.Execute();
+            }
+            else
+            {
+                MessageBox.Show("Показания уже вносились сегодня", "Ахтунг!", MessageBoxButtons.OK);
+            }
+            return sRange;
+        }
+
+        String GetPreDate(SheetsService service, string rowNumber)
+        {
+            String range = "Лист1!" + rowNumber;
+            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+
+                foreach (var row in values)
+                {
+                    foreach (var col in row)
+                    {
+                        range = col.ToString();
+                    }
+                }
+
+            return range;
         }
     }
 }
